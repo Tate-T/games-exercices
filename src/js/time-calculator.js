a92f17818bb217b6dd042fa447ed4dea124fbe19
artem1214 <61391756+artemanoh@users.noreply.github.com>
2025-02-22 12:11:44 +0200

    make leap year js and make time calculator scss and js

---
+++ src/js/time-calculator.js
@@ -0,0 +1,23 @@
+const timeInput = document.querySelector(".time-calculator-input");
+const timeOutput = document.querySelector(".time-calculator-time");
+const timeForm = document.querySelector(".time-calculator-form");
+
+timeForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const timeInSeconds = parseInt(timeInput.value);
+
+  if (Number.isInteger(timeInSeconds) && timeInSeconds >= 0) {
+    const days = Math.floor(timeInSeconds / 86400);
+    const hours = Math.floor((timeInSeconds % 86400) / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+
+    const formattedTime = `${days} дн. ${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+    timeOutput.textContent = formattedTime;
+  } else {
+    timeOutput.textContent = "Будь ласка, введіть коректне число.";
+  }
+
+  timeInput.value = "";
+});
