b56bb653fe44351fd0afe0fe5da0905e339a766e
artem1214 <61391756+artemanoh@users.noreply.github.com>
2025-02-28 19:43:06 +0200

    fixing bugs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import "./js/header.js" ;
-import "./js/google-dinosaur.js"
-import "./js/our-command.js"
-import "./js/footer-modal.js"
-import "./js/biggest-number.js"
-import "./js/open-modal.js"
-import "./js/calculator.js"
-import "./js/time-calculator.js"
-import "./js/leap-year.js"
-import "./js/rock-scissors-paper.js"
-import "./js/guess-number.js"
-import "./js/dark-mode.js"
-import "./js/choose-scientist.js"
-import "./js/football.js"\
+import "./js/header.js";
+import "./js/google-dinosaur.js";
+import "./js/our-command.js";
+import "./js/footer-modal.js";
+import "./js/biggest-number.js";
+import "./js/open-modal.js";
+import "./js/calculator.js";
+import "./js/time-calculator.js";
+import "./js/leap-year.js";
+import "./js/rock-scissors-paper.js";
+import "./js/guess-number.js";
+import "./js/dark-mode.js";
+import "./js/choose-scientist.js";
+import "./js/football.js";
 
