e9a8ca091e14623de53101f1f772b25ca1d6be21
MarkUA <mark.d.nahorniak@gmail.com>
2025-02-28 21:01:39 +0200

    ggg-modal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import "./js/open-modal.js"
 import "./js/header.js" ;
 import "./js/google-dinosaur.js"
 import "./js/our-command.js"
 import "./js/footer-modal.js"
 import "./js/biggest-number.js"
-import "./js/open-modal.js"
 import "./js/calculator.js"
 import "./js/time-calculator.js"
 import "./js/leap-year.js"
@@ -11,5 +11,4 @@ import "./js/rock-scissors-paper.js"
 import "./js/guess-number.js"
 import "./js/dark-mode.js"
 import "./js/choose-scientist.js"
-import "./js/football.js"\
-
+import "./js/football.js"
--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -1,6 +1,21 @@
-const closeOpenModalButton = document.querySelector("[data-open-modal-close]");
-const openModalBackdrop = document.querySelector(".open-modal-backdrop");
+const modalBackdrop = document.querySelector(".open-modal-backdrop");
+const closeModalButtons = document.querySelectorAll("[data-open-modal-close]");
+const inputField = document.querySelector(".open-modal__input");
+const saveButton = document.querySelector(".open-modal__button");
+const userNameSpan = document.querySelector(".header-hello-user span");
 
-closeOpenModalButton.addEventListener("click", (event) => {
-    openModalBackdrop.classList.add("open-modal-is-hidden");
+function closeModal() {
+  modalBackdrop.style.display = "none";
+}
+
+closeModalButtons.forEach(button => {
+  button.addEventListener("click", closeModal);
+});
+
+saveButton.addEventListener("click", () => {
+  const userName = inputField.value.trim();
+  if (userName) {
+    userNameSpan.textContent = userName;
+  }
+  closeModal();
 });
