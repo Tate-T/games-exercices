5a8fcab3c9c387e9a8c16a72b9262f2ea88570ff
artem1214 <61391756+artemanoh@users.noreply.github.com>
2025-03-09 12:35:06 +0200

    make

--- src/js/guess-number.js
+++ src/js/guess-number.js
@@ -14,13 +14,13 @@ checkButton.addEventListener("click", (event) => {
     const userGuess = parseInt(numberInput.value, 10);
     if (Number(userGuess) && userGuess >= 2 && userGuess <= 9) {
         if (userGuess === randomNumber) {
-            resultDisplay.textContent = Вітаю, ви вгадали число! (${randomNumber});
+            resultDisplay.textContent = "Вітаю, ви вгадали число! (${randomNumber})";
             resultDisplay.style.color = 'hsl(119, 100.00%, 30.00%)';
             resultDisplay.style.fontSize = '12px';
             resultDisplay.style.textAlign = 'right';
             resultDisplay.style.fontWeight = 400;  
         } else {
-            resultDisplay.textContent = Ви програли, комп’ютер загадав (${randomNumber});
+            resultDisplay.textContent = "Ви програли, комп’ютер загадав (${randomNumber})";
             resultDisplay.style.color = 'rgb(144, 9, 0)';
             resultDisplay.style.fontSize = '12px';
             resultDisplay.style.textAlign = 'right';
