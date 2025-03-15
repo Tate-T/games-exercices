878336af7aa774be5818da56e0def80146e70356
TheTureyProgramist <150546608+TheTureyProgramist@users.noreply.github.com>
2025-03-15 18:41:45 +0000

    Deploying to gh-pages from @ Tate-T/games-exercices@669b4497de5b61ca9819e5055e8b2b28d67a7e62 🚀

--- src/js/dark-mode.js
+++
@@ -1,5 +0,0 @@
-const themeToggleButton = document.querySelector('.dark-mode-change');
-themeToggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-
-});
--- src/js/scrolltotop.js
+++
@@ -1,9 +0,0 @@
-const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-
-
-scrollToTopBtn.addEventListener("click", function() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
